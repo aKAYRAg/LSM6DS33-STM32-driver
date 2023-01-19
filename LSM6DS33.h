14e1b4f11d05fcb8671219431a4736347d9ebcd9
aKAYRAg <111449192+aKAYRAg@users.noreply.github.com>
2023-01-19 13:13:12 +0300

    Update LSM6DS33.h

--- LSM6DS33.h
+++ LSM6DS33.h
@@ -1,13 +1,9 @@
 /*
  * LSM6DS33.h
  *
- *  Created on: 4 Ara 2022
+ *  Created on: Dec 21, 2022
  *      Author: kayra
  */
-
-#ifndef INC_LSM6DS33_H_
-#define INC_LSM6DS33_H_
-
 
 #include "stm32f4xx_hal.h"
 #include "stdint.h"
@@ -15,21 +11,18 @@
 
 extern I2C_HandleTypeDef hi2c1;
 
+#ifndef INC_LSM6DS33_H_
+#define INC_LSM6DS33_H_
+
+// LSM6DS33 init
 void LSM6DS33_init(void);
 
-int LSM6DS33_test(void);
-int LSM6DS33_testID(void);
+//gyro func.
+float read_data_gyro(int eksen);
 
-float LSM6DS33_Gyro_X(void);
-float LSM6DS33_Gyro_Y(void);
-float LSM6DS33_Gyro_Z(void);
-float LSM6DS33_Acc_X(void);
-float LSM6DS33_Acc_Y(void);
-float LSM6DS33_Acc_Z(void);
-
-float acc_total_vector(void);
+//acc func.
+float read_data_acc(int eksen);
 float angle_pitch_acc(void);
 float angle_roll_acc(void);
-
 
 #endif /* INC_LSM6DS33_H_ */
